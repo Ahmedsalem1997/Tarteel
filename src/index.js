975f6bde1bbbe7a06f16f25b5c840c985e5f5e17
Mahmoud taha <mahmoud.taha@idemia.com>
2023-01-31 21:18:43 +0200

    add carousel scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.scss';
 import App from './App';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
