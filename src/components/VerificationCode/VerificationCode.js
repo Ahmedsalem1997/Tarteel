9e78b083928a6ba20187b0e047ba1af4b94d6132
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-04 18:23:18 +0200

    translate verification code component

--- src/components/VerificationCode/VerificationCode.js
+++ src/components/VerificationCode/VerificationCode.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import LoginWrapper from "../LoginWrapper/LoginWrapper";
-
+import Translate from '../../helpers/Translate/Translate';
 const VerificationCode = () => {
   return (
     <LoginWrapper>
       <div className="verification-code-form-header">
-        <p>تم ارسال الرمز</p>
+        <p><Translate id="verificationCode.sent" /></p>
       </div>
       <div className="verification-code-form-inputs">
         <input type="text" className="trans-input" maxLength="1"></input>
@@ -15,11 +15,11 @@ const VerificationCode = () => {
       </div>
 
       <div className="verification-code-form-actions">
-        <button className="main-button">ارسال</button>
+        <button className="main-button"><Translate id="button.send" /></button>
       </div>
       <div className="verification-code-form-cancel">
         <Link className="cancel-btn" to="/">
-          الغاء
+          <Translate id="button.cancel"/>
         </Link>
       </div>
     </LoginWrapper>
