9a0bec525c0e17c75ba6d0a116ebbe76c1dd5fb4
shehabkhallaf <shehabkhallaf122@gmail.com>
2023-02-05 00:26:32 +0200

    edit not found page className and remove comments

--- src/views/NotFoundPage/NotFoundPage.js
+++ src/views/NotFoundPage/NotFoundPage.js
@@ -1,12 +1,8 @@
 const NotFoundPage = () => {
   return (
-    // <div className="page-not-found1">
-    //   <h2>404</h2>
-    //   <h6>Page Not Found</h6>
-    //   <p>We couldn't find what you were looking for.</p>
-    // </div>
-    <div className="page-not-found2">
-      <div className="page-not-found2-content">
+   
+    <div className="page-not-found">
+      <div className="page-not-found-content">
         <h2>404</h2>
         <h6>Page Not Found</h6>
         <p>We couldn't find what you were looking for.</p>
