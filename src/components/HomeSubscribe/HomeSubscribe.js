e5212e7ac07ce6f7c43b42ead8d90da8caac5fac
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-04 17:12:39 +0200

    translate home subscribtion

--- src/components/HomeSubscribe/HomeSubscribe.js
+++ src/components/HomeSubscribe/HomeSubscribe.js
@@ -1,10 +1,11 @@
+import {Translate} from '../../helpers/Translate/Translate';
 const HomeSubscribe = () => {
     return (
         <div className="home-subscribe">
-            <p className="home-subscribe-text">ادخل رقم جوالك للاشتراك</p>
+            <p className="home-subscribe-text"><Translate id="subscribeSection.title"/></p>
             <div className="home-subscribe-input-group">
                 <input className="home-subscribe-input" type="email" />
-                <button className="home-subscribe-send-btn">ارسل</button>
+                <button className="home-subscribe-send-btn"><Translate id="button.send"/></button>
             </div>
         </div>
     )
