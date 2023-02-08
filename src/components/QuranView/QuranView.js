7b3e824724170351cb1fa63f43f3736f1fea426d
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-08 11:29:51 +0200

    handle loading and errors in quran

--- src/components/QuranView/QuranView.js
+++ src/components/QuranView/QuranView.js
@@ -2,6 +2,7 @@ import { Fragment, useEffect, useState } from "react";
 import useHTTP from "../../hooks/use-http";
 import { useSelector } from "react-redux";
 import Translate from "../../helpers/Translate/Translate";
+import Loader from "../Loader/Loader";
 
 const QuranView = (props) => {
     const [surah, setSurah] = useState([]);
@@ -10,7 +11,7 @@ const QuranView = (props) => {
         return state.lang.globalLang;
     });
     useEffect(() => {
-        const surahUrl = lang === 'ar'? `${props.selectedSurah}/quran-simple` : `${props.selectedSurah}/en.ahmedali`;
+        const surahUrl = lang === 'ar' ? `${props.selectedSurah}/quran-simple` : `${props.selectedSurah}/en.ahmedali`;
         getSurah({ url: `surah/${surahUrl}` }, surahObj => {
             setSurah(surahObj.data);
         });
@@ -18,14 +19,22 @@ const QuranView = (props) => {
     return (
         <div className="quran-view">
             <div className="quran-view-surah">
-                <div className="quran-view-surah-openning"><Translate id="quran.openning"/></div>
-                {surah?.ayahs?.map(ayah => {
-                    return (
-                        <Fragment key={ayah.number}>
-                            {(lang === 'ar' && ayah.numberInSurah === 1 && props.selectedSurah !== 1) ? ayah.text.replace( 'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ' ,'') : ayah.text}&nbsp;<span>{ayah.numberInSurah}</span>&nbsp;
-                        </Fragment>
-                    )
-                })}
+                <div className="quran-view-surah-openning"><Translate id="quran.openning" /></div>
+                {
+                    isLoading ?
+                        <Loader />
+                        :
+                        error ?
+                            error
+                            :
+                            surah?.ayahs?.map(ayah => {
+                                return (
+                                    <Fragment key={ayah.number}>
+                                        {(lang === 'ar' && ayah.numberInSurah === 1 && props.selectedSurah !== 1) ? ayah.text.replace('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ', '') : ayah.text}&nbsp;<span>{ayah.numberInSurah}</span>&nbsp;
+                                    </Fragment>
+                                )
+                            })
+                }
             </div>
         </div>
     )
