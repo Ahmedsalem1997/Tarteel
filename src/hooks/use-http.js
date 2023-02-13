b4c3ba59ba3dc96d54ad8d3eac1b18848552baff
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-13 23:07:29 +0200

    upgrade use http to use then instead of async

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -4,35 +4,62 @@ import { useCallback, useState } from "react";
 const useHTTP = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    const sendRequest = useCallback(async (requestConfig, applyData) => {
+    // const sendRequest = useCallback(async (requestConfig, applyData) => {
+    //     let baseUrl = "http://ec2-34-246-200-235.eu-west-1.compute.amazonaws.com/api/v1/front/";
+    //     setIsLoading(true);
+    //     setError(null);
+    //     if (requestConfig.baseUrl) {
+    //         baseUrl = requestConfig.baseUrl;
+    //     }
+    //     try {
+    //         const response = await fetch(
+    //             baseUrl + requestConfig.url,
+    //             {
+    //                 method: requestConfig.method,
+    //                 headers: requestConfig.headers,
+    //                 body: requestConfig.method === 'POST' && requestConfig.headers['Content-Type'] === 'application/json' ? JSON.stringify(requestConfig.body) : requestConfig.body
+    //             }
+    //         );
+
+    //         if (!response.ok) {
+    //             throw new Error('Request Failed!');
+    //         }
+
+    //         const data = await response.json();
+    //         applyData(data);
+    //     } catch (err) {
+    //         setError(err.message || 'something went wrong');
+    //     }
+    //     setIsLoading(false);
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
+    const sendRequest = useCallback((requestConfig, applyData) => {
         let baseUrl = "http://ec2-34-246-200-235.eu-west-1.compute.amazonaws.com/api/v1/front/";
         setIsLoading(true);
         setError(null);
-        if (requestConfig.baseUrl) {
-            baseUrl = requestConfig.baseUrl;
-        }
-        try {
-            const response = await fetch(
-                baseUrl + requestConfig.url,
-                {
-                    method: requestConfig.method,
-                    headers: requestConfig.headers,
-                    body: requestConfig.method === 'POST' && requestConfig.headers['Content-Type'] === 'application/json' ? JSON.stringify(requestConfig.body) : requestConfig.body
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error('Request Failed!');
+        fetch(
+            baseUrl + requestConfig.url,
+            {
+                method: requestConfig.method,
+                headers: requestConfig.headers,
+                body: requestConfig.method === 'POST' && requestConfig.headers['Content-Type'] === 'application/json' ? JSON.stringify(requestConfig.body) : requestConfig.body
+            }
+        ).then(async res => {
+            const data = await res.json();
+            if (!res.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || res.statusText;
+                return Promise.reject(error);
             }
 
-            const data = await response.json();
             applyData(data);
-        } catch (err) {
-            setError(err.message || 'something went wrong');
-        }
+        }).catch(err => {
+            setError(err);
+            console.error('useHTTP Error', err);
+        })
         setIsLoading(false);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+
+    }, [])
     return {
         isLoading,
         error,
