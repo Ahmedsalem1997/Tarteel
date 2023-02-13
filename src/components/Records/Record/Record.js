f48944eb2ebfc56a9d5f1894275fc5f833f0588f
shehabkhallaf <shehabkhallaf122@gmail.com>
2023-02-13 21:44:11 +0200

    implement like for record

--- src/components/Records/Record/Record.js
+++ src/components/Records/Record/Record.js
@@ -4,10 +4,28 @@ import Translate from "../../../helpers/Translate/Translate";
 import AddComment from "../../Comments/AddComment/AddComment";
 import AudioPlayer from "./../../AudioPlayer/AudioPlayer";
 import Comments from "./../../Comments/Comments";
+import useHTTP from './../../../hooks/use-http';
 
 const Record = (props) => {
   const img = require("../../../assets/images/personal.png");
+  const { sendRequest:toggleLike } = useHTTP();
   const [showComments, setShowComments] = useState(false);
+  const token = useSelector((state) => {
+    return state.auth.token;
+  });
+  const likeBtnHandler =()=>{
+
+    toggleLike(
+      {
+      url : `records/${props?.record?.id}/reactions`,
+      headers : {'Authorization': `Bearer ${token}`,
+     'Content-Type' : 'application/json'
+    },
+      method : 'POST',
+      body : {'reaction' : props.record.is_liked ? `unlike` :`like` }
+    }
+    )
+  }
   const loggedUser = useSelector(state => {
     return state.auth.user;
   });
@@ -38,7 +56,7 @@ const Record = (props) => {
       <div className="post-feedback">
         <span className="post-feedback-likes-comments">
           <p>{props?.record?.likes_count}</p>
-          <i className="fa-regular fa-thumbs-up"></i>
+          <i onClick={likeBtnHandler} className={`${props?.record.is_liked ? 'fa-solid' : 'fa-regular'} fa-thumbs-up`}></i>
           <p>{props?.record?.comments_count}</p>
           <i className="fa-regular fa-comment" onClick={() => setShowComments(prev => !prev)}></i>
         </span>
