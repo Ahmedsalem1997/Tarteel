111a22a9a8ba46aaaa5c098a5497bdd93ea3ecbc
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-14 18:35:08 +0200

    fix like for latestRecords

--- src/components/LatestRecords/LatestRecords.js
+++ src/components/LatestRecords/LatestRecords.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import useHTTP from "../../hooks/use-http";
+import { getAuth } from "../../utils/Auth";
 import Records from "../Records/Records";
 
 const LatestRecords = () => {
     const [latestRecords, setLatestRecords] = useState([]);
     const { isLoading, error, sendRequest: getLatestRecords } = useHTTP();
+    const { token } = getAuth();
     useEffect(() => {
         getLatestRecords(
             {
                 url: 'records/latest',
-                method: 'GET'
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
             },
             data => {
                 setLatestRecords(data.data);
