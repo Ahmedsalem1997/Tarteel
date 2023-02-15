b65ae356775da89fd726c9fa4a6e33bd5503652a
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-16 00:13:23 +0200

    media query for popup black block

--- src/components/BlackBlock/BlackBlock.js
+++ src/components/BlackBlock/BlackBlock.js
@@ -1,7 +1,7 @@
 const BlackBlock = (props) => {
     // const styleWidth = props.width && `width: $props.width`;
     return (
-        <div style={{ width: props.width ? props.width : '530px' }} className="black-block">
+        <div style={{ width: props.width || '530px' }} className="black-block">
             {
                 props.showClose && <div className="close-btn" onClick={ props.onClose }>
                     <i className="fa-solid fa-xmark"></i>
