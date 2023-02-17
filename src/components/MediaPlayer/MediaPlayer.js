8a5e3a30532317befef6ecb21d038e479742d755
shehabkhallaf <shehabkhallaf122@gmail.com>
2023-02-17 22:16:58 +0200

    customiza mediaPlayer modal

--- src/components/MediaPlayer/MediaPlayer.js
+++ src/components/MediaPlayer/MediaPlayer.js
@@ -3,6 +3,7 @@ import ReactPlayer from 'react-player'
 import { useDispatch, useSelector } from 'react-redux';
 import { modalsActions } from '../../store/Modals/Modals';
 import Modal from '../Modal/Modal';
+import { Translate } from "../../helpers/Translate/Translate";
 
 
 const MediaPlayer = () => {
@@ -11,9 +12,26 @@ const MediaPlayer = () => {
         dispatch(modalsActions.closeMediaModal())
     }
     const record = useSelector(state => state.modals.record);
+    const lang = useSelector((state) => {
+        return state.lang.globalLang;
+      });
     return (
-        <Modal width="75%" showClose={true} onClose={closeMediaModal}>
-            <ReactPlayer url={record.file} controls />
+        <Modal showClose={true} onClose={closeMediaModal}>
+            <div className="text-center fs-1">
+          <span>
+            {lang === "ar"
+              ? record?.surah?.name
+              : record?.surah?.english_name}
+            &nbsp;
+          </span>
+          <span>
+            <Translate id="record.fromAyah" /> &nbsp;
+            {record?.from_ayah_number}&nbsp;
+            <Translate id="record.toAyah" /> &nbsp;
+            {record?.to_ayah_number}
+          </span>
+        </div>
+            <ReactPlayer height="5rem" url={record.file} controls />
         </Modal >
     );
 }
