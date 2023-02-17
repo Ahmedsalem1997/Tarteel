75fc4a242d7a0a0b6caa7b319b78f92e277f5255
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-17 20:23:01 +0200

    fix loaders with Modals portal

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Profile from './views/Profile/Profile';
 import Quran from './views/Quran/Quran';
 import Login from './views/Login/Login';
 import VerificationCode from './components/VerificationCode/VerificationCode';
-import EditProfile from './components/EditProfile/EditProfile';
+// import EditProfile from './components/EditProfile/EditProfile';
 import NotFoundPage from './views/NotFoundPage/NotFoundPage';
 import { Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { langActions } from './store/Lang/Lang';
-import AddNewRecord from './components/AddNewRecord/AddNewRecord';
-import NotRegistered from './components/NotRegistered/NotRegistered';
-import MediaPlayer from './components/MediaPlayer/MediaPlayer';
+// import AddNewRecord from './components/AddNewRecord/AddNewRecord';
+// import NotRegistered from './components/NotRegistered/NotRegistered';
+// import MediaPlayer from './components/MediaPlayer/MediaPlayer';
 import { checkAuthLoader } from './utils/Auth';
+import BasicLayout from './views/BasicLayout/BasicLayout';
 // import { authActions } from './store/Auth/Auth';
 // import { getAuth } from './utils/Auth';
 
@@ -21,7 +22,6 @@ function App() {
   const globalLang = useSelector((state) => {
     return state.lang.globalLang;
   });
-  const modals = useSelector(state => state.modals);
   const [lang, setLang] = useState(localStorage.getItem("lang"));
   const dispatch = useDispatch();
   const rootEle = document.getElementById("root-html");
@@ -53,31 +53,37 @@ function App() {
   }, [globalLang]);
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: < Home />
-    },
-    {
-      path: "login",
-      element: <Login />
-    },
-    {
-      path: "users/:id",
-      element: <Profile />,
-      loader: checkAuthLoader
-    },
-    {
-      path: "islamic",
-      element: <Islamic />,
-      loader: checkAuthLoader
-    },
-    {
-      path: "quran",
-      element: <Quran />,
-      loader: checkAuthLoader
-    },
-    {
-      path: "verification-code/:mobile",
-      element: <VerificationCode />
+      path: '/',
+      element: <BasicLayout />,
+      children: [
+        {
+          path: "/",
+          element: < Home />
+        },
+        {
+          path: "login",
+          element: <Login />
+        },
+        {
+          path: "users/:id",
+          element: <Profile />,
+          loader: checkAuthLoader
+        },
+        {
+          path: "islamic",
+          element: <Islamic />,
+          loader: checkAuthLoader
+        },
+        {
+          path: "quran",
+          element: <Quran />,
+          loader: checkAuthLoader
+        },
+        {
+          path: "verification-code/:mobile",
+          element: <VerificationCode />
+        }
+      ]
     },
     {
       path: "*",
@@ -87,12 +93,7 @@ function App() {
   ])
   return (
     <Fragment>
-      <RouterProvider router={router} >
-        {modals.editProfileModal && <EditProfile />}
-        {modals.addNewRecordModal && <AddNewRecord />}
-        {modals.loginModal && <NotRegistered />}
-        {modals.mediaModal && <MediaPlayer />}
-      </RouterProvider>
+      <RouterProvider router={router} />
     </Fragment>
 
   );
--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -18,4 +18,5 @@ export function checkAuthLoader() {
     if (!token) {
         return redirect('/login');
     }
+    return null;
 }
