84f2ec3b4219ea3612cb514f145c9670e4153386
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-18 21:42:02 +0200

    implement show more for records

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -8,10 +8,12 @@ const Records = (props) => {
     const { isLoading, error, sendRequest } = useHTTP();
     const [records, setRecords] = useState([]);
     const { token } = getAuth();
+    const [page, setPage] = useState(1);
+    const [totalRecords, setTotalRecords] = useState(0);
     const getRecords = () => {
         sendRequest(
             {
-                url: `records${props?.recordsUrl}`,
+                url: `records${props?.recordsUrl}?page=${page}&per_page=10`,
                 method: 'GET',
                 headers:
                 {
@@ -19,23 +21,30 @@ const Records = (props) => {
                 }
             },
             data => {
-                setRecords(data.data);
+                setRecords(prev => [...prev, ...data.data]);
+                setTotalRecords(data.meta.total);
             },
             err => {
-                
+
             }
         )
+    }
+    const onShowMore = () => {
+        setPage(prev => prev + 1);
     }
     useEffect(() => {
         getRecords();
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [page]);
     return (
         <Fragment>
             {isLoading && <Loader />}
             {records?.map((record) => {
                 return <Record key={record.id} record={record} />
             })}
+            <div className="show-more">
+                <button disabled={records.length >= totalRecords} className="main-button" onClick={onShowMore}>show more</button>
+            </div>
         </Fragment>
     )
 }
