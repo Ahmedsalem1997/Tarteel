9a31015ca5dae4a756bdf8aff5a4f0b16f94db23
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-19 16:38:31 +0200

    fix id naming for comment records

--- src/components/AudioRecord/AudioRecord.js
+++ src/components/AudioRecord/AudioRecord.js
@@ -5,19 +5,20 @@ import Translate from '../../helpers/Translate/Translate';
 const AudioRecord = (props) => {
     const [showRecording, setShowRecording] = useState(true);
     const recorderControls = useAudioRecorder();
+    const randomId = Math.floor(Math.random()*1000);
 
     const addAudioElement = (blob) => {
         const url = URL.createObjectURL(blob);
         const audio = document.createElement('audio');
         audio.src = url;
         audio.controls = true;
-        document.getElementById('audio').appendChild(audio);
+        document.getElementById(`audio${randomId}`).appendChild(audio);
         props.onRecordFinished(blob);
         setShowRecording(false);
     };
 
     return (
-        <button type="button" className='audio-record' id='audio'>
+        <button type="button" className='audio-record' id={`audio${randomId}`}>
             {props.showTitle ? !(!showRecording || recorderControls.isRecording) && <Translate id="button.startRecording" /> : ""}
             {showRecording && <AudioRecorder
                 onRecordingComplete={(blob) => addAudioElement(blob)}
