dc343d0dc6c93b0c7cf3855816d1068d77260288
ahmeddsalem <ahmed_20160018@fci.helwan.edu.eg>
2023-02-22 11:37:31 +0200

    updated popup ui issues

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,46 +1,79 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { Translate } from "../../helpers/Translate/Translate";
 import { Fragment } from "react";
-import { Link } from 'react-router-dom';
-import { langActions } from '../../store/Lang/Lang';
-import { getAuth } from '../../utils/Auth';
-import { modalsActions } from '../../store/Modals/Modals';
+import { Link } from "react-router-dom";
+import { langActions } from "../../store/Lang/Lang";
+import { getAuth } from "../../utils/Auth";
+import { modalsActions } from "../../store/Modals/Modals";
 const Carousel = () => {
   const dispatch = useDispatch();
-  const globalLang = useSelector(state => {
-    return state.lang.globalLang
+  const globalLang = useSelector((state) => {
+    return state.lang.globalLang;
   });
   const { isAuth } = getAuth();
 
-
-  const changeLang = () => {
-    if (globalLang === 'en') {
-      dispatch(langActions.translation({ type: 'translation', lang: 'ar' }));
+  const openAddNewRecordModal = () => {
+    if (!isAuth) {
+      dispatch(modalsActions.openLoginModal());
     } else {
-      dispatch(langActions.translation({ type: 'translation', lang: 'en' }));
+      dispatch(modalsActions.openAddNewRecordModal());
     }
-  }
+  };
+
+  // const changeLang = () => {
+  //   if (globalLang === "en") {
+  //     dispatch(langActions.translation({ type: "translation", lang: "ar" }));
+  //   } else {
+  //     dispatch(langActions.translation({ type: "translation", lang: "en" }));
+  //   }
+  // };
   const openLoginModal = () => {
     dispatch(modalsActions.openLoginModal());
-  }
+  };
   return (
     <Fragment>
-      <h2 className='carousel-content-title'><Translate id="carousel.title" /></h2>
-      <button className="register-now-btn" onClick={changeLang}><Translate id="button.startRecording" /></button>
+      <h2 className="carousel-content-title">
+        <Translate id="carousel.title" />
+      </h2>
+      <button className="register-now-btn" onClick={openAddNewRecordModal}>
+        <Translate id="button.startRecording" />
+      </button>
       <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
       </div>
 
       <div className="register-bar">
-        <p><Translate id="carousel.bar" /></p>
-        {!isAuth && <button onClick={openLoginModal} className="register-now-btn"><Translate id="button.subscribe" /></button>}
+        <p>
+          <Translate id="carousel.bar" />
+        </p>
+        {!isAuth && (
+          <button onClick={openLoginModal} className="register-now-btn">
+            <Translate id="button.subscribe" />
+          </button>
+        )}
         {/* {!isAuth && <Link to="/subscription-login"><button className="register-now-btn"><Translate id="button.subscribe" /></button></Link>} */}
       </div>
     </Fragment>
-
-  )
+  );
 };
 
 export default Carousel;
