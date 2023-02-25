a42c787ab33c121d7b382e70aafdee2f1cc9c91c
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-25 23:28:56 +0200

    subscription --> no action happen when enter mobile number  then press send

--- src/components/HomeSubscribe/HomeSubscribe.js
+++ src/components/HomeSubscribe/HomeSubscribe.js
@@ -1,12 +1,59 @@
+import { useState } from 'react';
 import Translate from '../../helpers/Translate/Translate';
+import useHTTP from '../../hooks/use-http';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { setLongTermToken } from "../../utils/Auth";
+
 const HomeSubscribe = () => {
+    const [mobileNumber, setMobileNumber] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const { isLoading, error, sendRequest } = useHTTP();
+
+    const mobileNumberChangeHandler = (e) => {
+        if (e.target.value.trim().length !== 12) {
+            setErrorMessage("mobileNumberMessage");
+            // return;
+        } else {
+            setErrorMessage("");
+        }
+        // if (e.target.value.trim().length === 12) {
+        // }
+        setMobileNumber(e.target.value);
+    };
+
+    const onLogin = () => {
+        if (mobileNumber.trim().length === 12) {
+            sendRequest(
+                {
+                    url: "subscribe",
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: {
+                        long_term_token: setLongTermToken(),
+                        operator_id: 1
+                    },
+                },
+                (data) => {
+                    // console.log(data);
+                    window.location.replace(data.data);
+                },
+                err => {
+
+                }
+            );
+        } else {
+            setErrorMessage("mobileNumberMessage");
+        }
+
+    }
     return (
         <div className="home-subscribe">
-            <p className="home-subscribe-text"><Translate id="subscribeSection.title"/></p>
+            <p className="home-subscribe-text"><Translate id="subscribeSection.title" /></p>
             <div className="home-subscribe-input-group">
-                <input className="home-subscribe-input" type="email" />
-                <button className="home-subscribe-send-btn"><Translate id="button.send"/></button>
+                <input value={mobileNumber} className="home-subscribe-input" onChange={mobileNumberChangeHandler} type="number" />
+                <button disabled={errorMessage} className="home-subscribe-send-btn" onClick={onLogin}><Translate id="button.send" /></button>
             </div>
+            <ErrorMessage message={errorMessage} />
         </div>
     )
 }
