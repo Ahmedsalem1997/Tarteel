aa6766917321c4872b2c926e58aa45f6f15e666d
Mahmoud taha <mahmoud.taha@idemia.com>
2023-02-27 01:40:12 +0200

    remove error mesaage div if there is no error

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,13 +1,17 @@
 import Translate from "../../helpers/Translate/Translate";
 
 const ErrorMessage = ({ message }) => {
-    return (
-        <div className="err-message">
-            <p>
-                <Translate id={message ? `error.${message}`:''} />
-            </p>
-        </div>
-    );
+    if (message) {
+        return (
+            <div className="err-message">
+                <p>
+                    <Translate id={message ? `error.${message}` : ''} />
+                </p>
+            </div>
+        );
+    } else {
+        return;
+    }
 }
 
 export default ErrorMessage;
