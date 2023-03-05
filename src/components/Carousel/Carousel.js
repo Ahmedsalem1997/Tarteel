0595296d6c2550010808fc9949fbf7d489ae2b9f
Mahmoud taha <mahmoud.taha@idemia.com>
2023-03-05 12:11:41 +0200

    edit carousel image and gradient

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -46,6 +46,12 @@ const Carousel = () => {
           data-bs-slide-to="2"
           aria-label="Slide 3"
         ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
       </div>
 
       <div className="register-bar">
--- src/components/CarouselWrapper/CarouselWrapper.js
+++ src/components/CarouselWrapper/CarouselWrapper.js
@@ -1,9 +1,10 @@
 import Navigation from "../Navigation/Navigation";
 
 const CarouselWrapper = (props) => {
-    const img1 = require("../../assets/images/Group 3753.webp");
-    const img2 = require("../../assets/images/Group 3753@2x.webp");
-    const img3 = require("../../assets/images/Group 3753@3x.webp");
+    const img1 = require("../../assets/images/bg1.png");
+    const img2 = require("../../assets/images/bg2.png");
+    const img3 = require("../../assets/images/bg3.png");
+    const img4 = require("../../assets/images/bg4.png");
     return (
         <div id="carouselExampleIndicators" className="carousel slide">
             <div className="carousel-inner">
@@ -15,6 +16,9 @@ const CarouselWrapper = (props) => {
                 </div>
                 <div className="carousel-item">
                     <img src={img3} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src={img4} className="d-block w-100" alt="..." />
                 </div>
             </div>
             <div className="carousel-content">
