355042acb9e138ec241513f323f5df3a0a58f329
Mahmoud taha <mahmoud.taha@idemia.com>
2023-03-06 16:08:11 +0200

    implement new recording for add new record

--- src/components/Comments/AddComment/AddComment.js
+++ src/components/Comments/AddComment/AddComment.js
@@ -7,20 +7,13 @@ import { getAuth } from "../../../utils/Auth";
 import Loader from "../../Loader/Loader";
 import { useDispatch } from "react-redux";
 import { modalsActions } from "../../../store/Modals/Modals";
-// import { useAudioRecorder, AudioRecorder } from "react-audio-voice-recorder";
 import ErrorMessage from '../../ErrorMessage/ErrorMessage';
-// import MicRecorder from 'mic-recorder-to-mp3';
 import AudioRecord from '../../AudioRecord/AudioRecord';
 
 
 const AddComment = (props) => {
-  // const [Mp3Recorder, setMp3Recorder] = useState(
-  //   new MicRecorder({ bitRate: 128 })
-  // );
-  // const [recordingTimeInterval, setRecordingTimeInterval] = useState();
   const [comment, setComment] = useState('');
   const { isLoading, error, sendRequest: addComment } = useHTTP();
-  // const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 1000));
   const auth = getAuth();
   const [commentErr, setCommentErr] = useState('');
   const [uploadedRecord, setUploadedRecord] = useState(undefined);
@@ -28,19 +21,6 @@ const AddComment = (props) => {
   const openLoginModal = () => {
     dispatch(modalsActions.openLoginModal());
   }
-  // const [isMp3Recording, setIsMp3Recording] = useState(false);
-  // const [recordingMp3Time, setRecordingMp3Time] = useState(0);
-  // const [isBlocked, setIsBlocked] = useState(false);
-  // const [blobUrl, setBlobUrl] = useState('');
-  // const {
-  //   startRecording,
-  //   stopRecording,
-  //   togglePauseResume,
-  //   recordingBlob,
-  //   isRecording,
-  //   isPaused,
-  //   recordingTime,
-  // } = useAudioRecorder();
   const addCommentHandler = (e) => {
     e.preventDefault();
     let formData = new FormData();
@@ -68,33 +48,6 @@ const AddComment = (props) => {
           }
         }
       }
-
-      // if (comment.trim().length > 3) {
-      //   formData.append('text', comment);
-      //   if (uploadedRecord) {
-      //     formData.append('file', uploadedRecord);
-      //   }
-      //   addComment(
-      //     {
-      //       url: `records/${props.recordId}/comments`,
-      //       method: 'POST',
-      //       headers: {
-      //         'Authorization': `Bearer ${auth.token}`
-      //       },
-      //       body: formData
-      //     },
-      //     data => {
-      //       // console.log(data);
-      //       setComment('');
-      //       props.onAddComment();
-      //     },
-      //     err => {
-
-      //     }
-      //   )
-      // } else {
-      //   setCommentErr('comment');
-      // }
     } else {
       openLoginModal();
     }
@@ -124,18 +77,6 @@ const AddComment = (props) => {
     setUploadedRecord(blob);
   }
 
-  // const handlerRecordBtn = () => {
-  //   if (!isRecording && !recordingBlob) {
-  //     startRecording();
-  //   }
-  //   // console.log(recordingBlob, recordingTime);
-  // }
-
-  // const handleStopRecording = () => {
-  //   stopRecording();
-  //   // setUploadedRecord(recordingBlob);
-  // }
-
   const onChangeComment = (e) => {
     const schema = Joi.object({
       comment: Joi.string()
@@ -152,45 +93,6 @@ const AddComment = (props) => {
     }
     setComment(e.target.value);
   }
-
-  // const start = () => {
-  //   if (!isMp3Recording) {
-  //     navigator.getUserMedia({ audio: true },
-  //       () => {
-  //         console.log('Permission Granted');
-  //         setIsBlocked(false);
-  //         Mp3Recorder
-  //           .start()
-  //           .then(() => {
-  //             setIsMp3Recording(true);
-  //             setRecordingTimeInterval(setInterval(() => {
-  //               setRecordingMp3Time(prev => prev + 1);
-  //             }, 1000));
-  //           }).catch((e) => console.error(e));
-  //       },
-  //       () => {
-  //         console.log('Permission Denied');
-  //         setIsBlocked(true);
-  //       },
-  //     );
-  //   }
-  // };
-
-  // const stop = (e) => {
-  //   e.stopPropagation()
-  //   // e.stopPropgation()
-  //   clearInterval(recordingTimeInterval);
-  //   setRecordingMp3Time(0);
-  //   Mp3Recorder
-  //     .stop()
-  //     .getMp3()
-  //     .then(([buffer, blob]) => {
-  //       const blobURL = URL.createObjectURL(blob);
-  //       setBlobUrl(blobURL);
-  //       setIsMp3Recording(false);
-  //       setUploadedRecord(blob);
-  //     }).catch((e) => console.log(e));
-  // };
   return (
     <Fragment>
       {isLoading && <Loader />}
