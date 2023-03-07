3a332e6a6b6483ae29494e59eda4d06e429ff7a3
Mahmoud taha <mahmoud.taha@idemia.com>
2023-03-07 13:41:23 +0200

    conver class to className on naviagtion

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -15,36 +15,36 @@ const Navigation = () => {
     //   <li><NavLink to="/quran" activeclassname="active"><Translate id="navigation.quran" /></NavLink></li>
     // </ul>
     <div className="navigation-links">
-      <nav class="navbar navbar-expand-md navbar-dark">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#naviagtion" aria-controls="naviagtion" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-md navbar-dark">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#naviagtion" aria-controls="naviagtion" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="naviagtion">
-            <ul class="navbar-nav">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="naviagtion">
+            <ul className="navbar-nav">
+              <li className="nav-item">
                 <NavLink to="/home" activeclassname="active"><Translate id="navigation.home" /></NavLink>
               </li>
               {
                 (auth?.isAuth && !auth?.loggedUser?.is_sheikh) &&
-                <li class="nav-item">
+                <li className="nav-item">
                   <NavLink to={`/users/${auth.loggedUser.id}`} activeclassname="active"><Translate id="navigation.myRecords" /></NavLink>
                 </li>
               }
               {
                 !auth?.loggedUser?.is_sheikh &&
                 <Fragment>
-                  <li class="nav-item">
+                  <li className="nav-item">
                     <NavLink to="/islamic" activeclassname="active"><Translate id="navigation.islamicContent" /></NavLink>
                   </li>
-                  <li class="nav-item">
+                  <li className="nav-item">
                     <NavLink to="/quran" activeclassname="active"><Translate id="navigation.quran" /></NavLink>
                   </li>
                 </Fragment>
               }
               {
                 auth.isAuth &&
-                <li class="nav-item">
+                <li className="nav-item">
                   <Link to="/" onClick={logout}><Translate id="navigation.logout" /></Link>
                 </li>
               }
